9661d3b4a655cf409c829c4070862e7c7092e3e8
amustyy <geloservices@outlook.com>
2024-10-12 21:18:15 +1100

    added address to contacts table

--- frontend/src/components/NewPhone.js
+++ frontend/src/components/NewPhone.js
@@ -38,7 +38,6 @@ function NewPhone(props) {
                 <option value="Mobile">Mobile</option>
                 <option value="Other" >Other</option>
             </select>
-            {/* <input type='text' placeholder='Name' onChange={(e) => setName(e.target.value)} value={name}/> */}
             <input type='text' placeholder='Phone Number' onChange={(e) => setNumber(e.target.value)} value={number}/>
             <button className='button green' type='submit'>Add Choiru's Phone</button>
         </form>
